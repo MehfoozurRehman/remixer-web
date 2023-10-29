07d72437268400f99d9d616d128096e3196e01eb
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-10-29 22:49:13 +0500

    Update App.jsx

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,16 +1,5 @@
-import { Head } from '@router';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-export default () => {
-  return (
-    <>
-      <Head
-        title="project_name"
-        image="/favicon.ico"
-        url="https://vitefilerouter.com"
-        description="project_name"
-      />
-      <Outlet />
-    </>
-  );
-};
+export default function App() {
+  return <Outlet />;
+}
