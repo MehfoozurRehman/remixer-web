9b936d0075dd9f5e8b45d088e40a1a83b7799b46
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-10-29 22:49:21 +0500

    Update Error.jsx

--- src/layouts/Error.jsx
+++ src/layouts/Error.jsx
@@ -1,6 +1,6 @@
 import { useRouteError } from "react-router-dom";
 
-export default () => {
+export default function Error() {
   const error = useRouteError();
   return <div>{error?.message ?? "An error occurred"}</div>;
-};
+}
