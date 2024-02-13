2fd84a89b23622a37f3120f57538f22557ce9a39
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-13 09:50:29 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -57,7 +57,7 @@ try {
 export default defineConfig({
   resolve: { alias },
   plugins: [
-    config.autoExport && HotExport(),
+    HotExport(),
     config.compression && [
       chunkSplitPlugin(),
       viteCompression({
