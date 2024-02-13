8a2f209d439dec1efdd358c5a1bd32afde114452
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-13 09:50:47 +0500

    Update Link.jsx

--- router/Link.jsx
+++ router/Link.jsx
@@ -3,24 +3,29 @@ import { memo, useRef } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import usePrefetchLink from "./usePrefetchLink";
 
-export default memo(({ to, prefetch = true, className, ...props }) => {
+const Link = memo(({ to, prefetch = true, className, ...props }) => {
   const ref = useRef(null);
   const { handleMouseEnter } = usePrefetchLink(to, prefetch);
+
+  const linkClassName = ({ isActive, isPending, isTransitioning }) => {
+    const classNames = [
+      isPending ? "pending" : "",
+      isActive ? "active" : "",
+      isTransitioning ? "transitioning" : "",
+      className || "",
+    ];
+    return classNames.join(" ");
+  };
 
   return (
     <RouterLink
       ref={ref}
       to={to}
       onMouseEnter={handleMouseEnter}
-      className={({ isActive, isPending, isTransitioning }) =>
-        [
-          isPending ? "pending" : "",
-          isActive ? "active" : "",
-          isTransitioning ? "transitioning" : "",
-          className || "",
-        ].join(" ")
-      }
+      className={linkClassName}
       {...props}
     />
   );
 });
+
+export default Link;
