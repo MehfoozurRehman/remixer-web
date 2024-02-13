ac25b1ed57f6fc13b9ae5cd32d15fbc1a8c8fa89
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-13 09:50:42 +0500

    Update Head.jsx

--- router/Head.jsx
+++ router/Head.jsx
@@ -1,23 +1,24 @@
-import Helmet from "react-helmet";
-import { useMemo } from "react";
+import { memo, useMemo } from "react";
 
-export default function Head({
-  title,
-  description,
-  url,
-  image,
-  children,
-  type,
-  siteName,
-  domain,
-  card,
-  author,
-  keywords,
-  robots,
-}) {
-  const metaTags = useMemo(
-    () =>
-      [
+import { Helmet } from "react-helmet";
+
+const Head = memo(
+  ({
+    title,
+    description,
+    url,
+    image,
+    children,
+    type,
+    siteName,
+    domain,
+    card,
+    author,
+    keywords,
+    robots,
+  }) => {
+    const metaTags = useMemo(() => {
+      const tags = [
         { name: "title", content: title },
         { name: "description", content: description },
         { name: "url", content: url },
@@ -29,12 +30,17 @@ export default function Head({
         { property: "og:description", content: description },
         { property: "og:url", content: url },
         { property: "og:image", content: image },
-        type && { property: "og:type", content: type },
-        siteName && { property: "og:site_name", content: siteName },
-        domain && { property: "twitter:domain", content: domain },
-        card && { property: "twitter:card", content: card },
-      ].filter(Boolean),
-    [
+        { property: "og:type", content: type },
+        { property: "og:site_name", content: siteName },
+        { property: "twitter:domain", content: domain },
+        { property: "twitter:card", content: card },
+        { name: "twitter:title", content: title },
+        { name: "twitter:description", content: description },
+        { name: "twitter:image", content: image },
+      ];
+
+      return tags;
+    }, [
       title,
       description,
       url,
@@ -46,18 +52,20 @@ export default function Head({
       author,
       keywords,
       robots,
-    ]
-  );
+    ]);
 
-  return (
-    <Helmet>
-      {metaTags.map((tag, index) => (
-        <meta key={tag.name || tag.property || index} {...tag} />
-      ))}
-      <link rel="icon" href={image} />
-      <link rel="apple-touch-icon" href={image} />
-      <meta name="theme-color" content="#000000" />
-      {children}
-    </Helmet>
-  );
-}
+    return (
+      <Helmet>
+        {metaTags.map((tag, index) => (
+          <meta key={tag.name || tag.property || index} {...tag} />
+        ))}
+        <link rel="icon" href={image} />
+        <link rel="apple-touch-icon" href={image} />
+        <meta name="theme-color" content="#000000" />
+        {children}
+      </Helmet>
+    );
+  }
+);
+
+export default Head;
