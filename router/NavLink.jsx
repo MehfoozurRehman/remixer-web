97639c35b75fbf385f528fc9fcd1bc3a775df6c3
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-13 17:41:20 +0500

    Update NavLink.jsx

--- router/NavLink.jsx
+++ router/NavLink.jsx
@@ -3,26 +3,15 @@ import { memo, useRef } from "react";
 import { NavLink as RouterNavLink } from "react-router-dom";
 import usePrefetchLink from "./usePrefetchLink";
 
-const NavLink = memo(({ to, prefetch = true, className, ...props }) => {
+const NavLink = memo(({ to, prefetch = true, ...props }) => {
   const ref = useRef(null);
   const { handleMouseEnter } = usePrefetchLink(to, prefetch);
-
-  const linkClassName = ({ isActive, isPending, isTransitioning }) => {
-    const classNames = [
-      isPending ? "pending" : "",
-      isActive ? "active" : "",
-      isTransitioning ? "transitioning" : "",
-      className || "",
-    ];
-    return classNames.join(" ");
-  };
 
   return (
     <RouterNavLink
       ref={ref}
       to={to}
       onMouseEnter={handleMouseEnter}
-      className={linkClassName}
       {...props}
     />
   );
