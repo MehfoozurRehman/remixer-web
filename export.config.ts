99f236431dac9a8a62651978cb0a0439df051333
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-13 17:57:03 +0500

    Update export.config.ts

--- export.config.ts
+++ export.config.ts
@@ -1,15 +1,22 @@
 import { config } from "./remix.config.js";
 import { defineExportConfig } from "vite-plugin-hot-export";
 
+const defaultConfigs = [
+  config.autoExportComponents && {
+    targetDir: "./src/components",
+  },
+  config.autoExportAssets && {
+    targetDir: "./src/assets",
+    autoPrefix: true,
+    depth: true,
+  },
+].filter(Boolean);
+
+const routeConfigs =
+  config.autoExports?.map((route: { path: any }) => ({
+    targetDir: route.path,
+  })) || [];
+
 export default defineExportConfig({
-  configs: [
-    config.autoExportComponents && {
-      targetDir: "./src/components",
-    },
-    config.autoExportAssets && {
-      targetDir: "./src/assets",
-      autoPrefix: true,
-      depth: true,
-    },
-  ].filter(Boolean),
+  configs: [...defaultConfigs, ...routeConfigs],
 });
