ae86d7cfadaceacbb872ff87ae1372a7eee0794e
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-13 09:50:22 +0500

    Update export.config.ts

--- export.config.ts
+++ export.config.ts
@@ -1,14 +1,15 @@
+import { config } from "./remix.config.js";
 import { defineExportConfig } from "vite-plugin-hot-export";
 
 export default defineExportConfig({
   configs: [
-    {
+    config.autoExportComponents && {
       targetDir: "./src/components",
     },
-    {
+    config.autoExportAssets && {
       targetDir: "./src/assets",
       autoPrefix: true,
       depth: true,
     },
-  ],
+  ].filter(Boolean),
 });
