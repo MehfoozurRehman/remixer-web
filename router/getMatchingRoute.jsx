68a51b3bf6c1e3f097f16645cf17ea74759ecde7
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-13 09:50:39 +0500

    Update getMatchingRoute.jsx

--- router/getMatchingRoute.jsx
+++ router/getMatchingRoute.jsx
@@ -3,11 +3,15 @@ import { LazyRoutes } from "./Router";
 const regexCache = new Map();
 
 export default function getMatchingRoute(path) {
+  let regex;
+
   for (const route of LazyRoutes) {
-    let regex = regexCache.get(route.path);
+    regex = regexCache.get(route.path);
 
     if (!regex) {
-      const newPath = route.path.replace(/:[^/]+/g, "([^/]+)").replace(/\*/g, ".*");
+      const newPath = route.path
+        .replace(/:[^/]+/g, "([^/]+)")
+        .replace(/\*/g, ".*");
       regex = new RegExp(`^${newPath}$`);
       regexCache.set(route.path, regex);
     }
