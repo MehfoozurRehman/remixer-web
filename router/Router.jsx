a462a0755493793a8597df581efa8eeec066684e
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-13 17:43:16 +0500

    Update Router.jsx

--- router/Router.jsx
+++ router/Router.jsx
@@ -17,46 +17,31 @@ const EAGER_ROUTES = import.meta.glob(
 
 const getAction = async (module, ...args) => {
   const { action } = await module();
-
-  if (action && typeof action === "function") {
-    return action(...args);
-  }
-
-  return null;
+  return action && typeof action === "function" ? action(...args) : null;
 };
 
 const getLoader = async (module, ...args) => {
   const { loader } = await module();
-
-  if (loader && typeof loader === "function") {
-    return loader(...args);
-  }
-
-  return null;
+  return loader && typeof loader === "function" ? loader(...args) : null;
 };
 
 const createRoute = (module, isEager) => {
   const Component = isEager ? module.default : lazy(() => module);
-
   const element = Component ? <Component /> : <Fragment />;
   const errorElement = <ErrorBoundary />;
-
   const preload = isEager ? null : module;
   const loader = isEager ? module?.loader : getLoader.bind(null, module);
   const action = isEager ? module?.action : getAction.bind(null, module);
-
   return { element, loader, action, preload, errorElement };
 };
 
 const createPathSegments = (key) => {
   const lowerCasePath = key.toLowerCase();
-
   if (key === lowerCasePath) {
     const errorMessage = `Path "${key}" is in lowercase.`;
     alert(errorMessage);
     throw new Error(errorMessage);
   }
-
   return key
     .replace(/\/src\/screens|\.jsx|\[\.{3}.+\]|\.lazy/g, "")
     .replace(/\[(.+?)\]/g, ":$1")
@@ -67,13 +52,11 @@ const createPathSegments = (key) => {
 
 const insertRoute = (routes, segments, route) => {
   const insert = /^\w|\//.test(route.path) ? "unshift" : "push";
-
   const insertNode = (parent, segment, index) => {
     const path = segment.replace(/index|\./g, "");
     const root = index === 0;
     const leaf = index === segments.length - 1 && segments.length > 1;
     const node = !root && !leaf;
-
     if (root) {
       const dynamic = path.startsWith("[") || path === "*";
       if (dynamic) return parent;
@@ -83,7 +66,6 @@ const insertRoute = (routes, segments, route) => {
         return parent;
       }
     }
-
     if (root || node) {
       const current = root ? routes : parent.children;
       const found = current?.find((route) => route.path === path);
@@ -92,42 +74,28 @@ const insertRoute = (routes, segments, route) => {
         : current?.[insert]({ path: path, children: [] });
       return found || current?.[insert === "unshift" ? 0 : current.length - 1];
     }
-
     if (leaf) {
       parent?.children?.[insert]({ path: path.replace(/\/$/, ""), ...route });
     }
-
     return parent;
   };
-
   segments.reduce(insertNode, {});
 };
 
-const createEagerRoutes = (eagers) => {
-  const routes = Object.keys(eagers).reduce((routes, key) => {
-    const route = createRoute(eagers[key], true);
+const createRoutes = (routes, modules, isEager) => {
+  const keys = Object.keys(modules);
+  const length = keys.length;
+  for (let i = 0; i < length; i++) {
+    const key = keys[i];
+    const route = createRoute(modules[key], isEager);
     const segments = createPathSegments(key);
     insertRoute(routes, segments, route);
-    return routes;
-  }, []);
-
+  }
   return routes;
 };
 
-const createLazyRoutes = (lazys) => {
-  const routes = Object.keys(lazys).reduce((routes, key) => {
-    const route = createRoute(lazys[key], false);
-    const segments = createPathSegments(key);
-    insertRoute(routes, segments, route);
-    return routes;
-  }, []);
-
-  return routes;
-};
-
-const EagerRoutes = createEagerRoutes(EAGER_ROUTES);
-
-export const LazyRoutes = createLazyRoutes(LAZY_ROUTES);
+const EagerRoutes = createRoutes([], EAGER_ROUTES, true);
+export const LazyRoutes = createRoutes([], LAZY_ROUTES, false);
 
 const router = createBrowserRouter([
   {
