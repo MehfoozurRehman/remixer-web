8b43752ed8048b58208dec8e9cb62bc0ec0159e8
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-27 16:36:55 +0500

    Update SuspenseAfterInitialRender.jsx

--- router/SuspenseAfterInitialRender.jsx
+++ router/SuspenseAfterInitialRender.jsx
@@ -1,14 +1,18 @@
 import { Suspense, memo, useRef } from "react";
 
-const SuspenseAfterInitialRender = memo(({ fallback, children }) => {
-  const isInitialRenderRef = useRef(true);
+import Loading from "@layouts/Loading";
 
-  if (isInitialRenderRef.current) {
-    isInitialRenderRef.current = false;
-    return <>{children}</>;
+const SuspenseAfterInitialRender = memo(
+  ({ fallback = <Loading />, children }) => {
+    const isInitialRenderRef = useRef(true);
+
+    if (isInitialRenderRef.current) {
+      isInitialRenderRef.current = false;
+      return <>{children}</>;
+    }
+
+    return <Suspense fallback={fallback}>{children}</Suspense>;
   }
-
-  return <Suspense fallback={fallback}>{children}</Suspense>;
-});
+);
 
 export default SuspenseAfterInitialRender;
