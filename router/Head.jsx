82123e7b6fbe5f108fa614492547dbe4e8814e35
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-02-28 09:20:58 +0500

    Update Head.jsx

--- router/Head.jsx
+++ router/Head.jsx
@@ -1,71 +1,22 @@
-import { memo, useMemo } from "react";
-
 import { Helmet } from "react-helmet";
+import { memo } from "react";
 
-const Head = memo(
-  ({
-    title,
-    description,
-    url,
-    image,
-    children,
-    type,
-    siteName,
-    domain,
-    card,
-    author,
-    keywords,
-    robots,
-  }) => {
-    const metaTags = useMemo(() => {
-      const tags = [
-        { name: "title", content: title },
-        { name: "description", content: description },
-        { name: "url", content: url },
-        { name: "image", content: image },
-        { name: "author", content: author },
-        { name: "keywords", content: keywords },
-        { name: "robots", content: robots },
-        { property: "og:title", content: title },
-        { property: "og:description", content: description },
-        { property: "og:url", content: url },
-        { property: "og:image", content: image },
-        { property: "og:type", content: type },
-        { property: "og:site_name", content: siteName },
-        { property: "twitter:domain", content: domain },
-        { property: "twitter:card", content: card },
-        { name: "twitter:title", content: title },
-        { name: "twitter:description", content: description },
-        { name: "twitter:image", content: image },
-      ];
-
-      return tags;
-    }, [
-      title,
-      description,
-      url,
-      image,
-      type,
-      siteName,
-      domain,
-      card,
-      author,
-      keywords,
-      robots,
-    ]);
-
-    return (
-      <Helmet>
-        {metaTags.map((tag, index) => (
-          <meta key={tag.name || tag.property || index} {...tag} />
-        ))}
-        <link rel="icon" href={image} />
-        <link rel="apple-touch-icon" href={image} />
-        <meta name="theme-color" content="#000000" />
-        {children}
-      </Helmet>
-    );
-  }
+const Head = ({ title, description, url, image, children }) => (
+  <Helmet>
+    <meta name="title" content={title} />
+    <meta name="description" content={description} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:url" content={url} />
+    <meta property="og:image" content={image} />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content={image} />
+    <link rel="icon" href={image} />
+    <link rel="apple-touch-icon" href={image} />
+    <meta name="theme-color" content="#000000" />
+    {children}
+  </Helmet>
 );
 
-export default Head;
+export default memo(Head);
