30a73400bbb52b3b53faa108a623d596203586d0
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-14 00:37:42 +0500

    Update Link.jsx

--- remixed/Link.jsx
+++ remixed/Link.jsx
@@ -5,12 +5,11 @@ import usePrefetchLink from "@/router/usePrefetchLink";
 
 const Link = ({ to, prefetch = true, ...props }) => {
   const ref = useRef(null);
-
   const prefetchLink = usePrefetchLink(to, prefetch);
 
   const handleMouseEnter = useCallback(() => {
     prefetchLink.handleMouseEnter();
-  }, [to, prefetch]);
+  }, [prefetchLink]);
 
   return (
     <RouterLink ref={ref} to={to} onMouseEnter={handleMouseEnter} {...props} />
