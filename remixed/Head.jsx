69ab59cd63c36340bd1360b6b574ef62cd5d3216
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-14 00:33:35 +0500

    Update Head.jsx

--- remixed/Head.jsx
+++ remixed/Head.jsx
@@ -1,22 +1,30 @@
 import { Helmet } from "react-helmet";
 import { memo } from "react";
 
-const Head = ({ title, description, url, image, children }) => (
-  <Helmet>
-    <meta name="title" content={title} />
-    <meta name="description" content={description} />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    <meta property="og:url" content={url} />
-    <meta property="og:image" content={image} />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-    <meta name="twitter:image" content={image} />
-    <link rel="icon" href={image} />
-    <link rel="apple-touch-icon" href={image} />
-    <meta name="theme-color" content="#000000" />
-    {children}
-  </Helmet>
-);
+const Head = ({ title, description, url, image, children }) => {
+  const metaTags = [
+    { name: "title", content: title },
+    { name: "description", content: description },
+    { property: "og:title", content: title },
+    { property: "og:description", content: description },
+    { property: "og:url", content: url },
+    { property: "og:image", content: image },
+    { name: "twitter:title", content: title },
+    { name: "twitter:description", content: description },
+    { name: "twitter:image", content: image },
+    { name: "theme-color", content: "#000000" },
+  ].filter((tag) => tag.content);
+
+  return (
+    <Helmet>
+      {metaTags.map((tag, index) => (
+        <meta key={index} {...tag} />
+      ))}
+      <link rel="icon" href={image} />
+      <link rel="apple-touch-icon" href={image} />
+      {children}
+    </Helmet>
+  );
+};
 
 export default memo(Head);
