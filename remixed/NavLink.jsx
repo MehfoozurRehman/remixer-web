1b78a6fb8e041849ce7583bbd171c886afa50c2c
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-14 00:30:19 +0500

    Create NavLink.jsx

---
+++ remixed/NavLink.jsx
@@ -0,0 +1,20 @@
+import { memo, useCallback, useRef } from "react";
+
+import { NavLink as RouterLink } from "react-router-dom";
+import usePrefetchLink from "@/router/usePrefetchLink";
+
+const NavLink = ({ to, prefetch = true, ...props }) => {
+  const ref = useRef(null);
+
+  const prefetchLink = usePrefetchLink(to, prefetch);
+
+  const handleMouseEnter = useCallback(() => {
+    prefetchLink.handleMouseEnter();
+  }, [to, prefetch]);
+
+  return (
+    <RouterLink ref={ref} to={to} onMouseEnter={handleMouseEnter} {...props} />
+  );
+};
+
+export default memo(NavLink);
