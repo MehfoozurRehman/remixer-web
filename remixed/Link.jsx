d303ead6b301e1bb972dfa35a8ec1f11ab3796fe
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-14 00:30:13 +0500

    Create Link.jsx

---
+++ remixed/Link.jsx
@@ -0,0 +1,20 @@
+import { memo, useCallback, useRef } from "react";
+
+import { Link as RouterLink } from "react-router-dom";
+import usePrefetchLink from "@/router/usePrefetchLink";
+
+const Link = ({ to, prefetch = true, ...props }) => {
+  const ref = useRef(null);
+
+  const prefetchLink = usePrefetchLink(to, prefetch);
+
+  const handleMouseEnter = useCallback(() => {
+    prefetchLink.handleMouseEnter();
+  }, [to, prefetch]);
+
+  return (
+    <RouterLink ref={ref} to={to} onMouseEnter={handleMouseEnter} {...props} />
+  );
+};
+
+export default memo(Link);
