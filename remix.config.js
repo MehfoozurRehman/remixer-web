d02276874ffecf72333e93aea3e8eed816a3cd27
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2026-09-15 22:23:14 +0500

    Revert "[ImgBot] Optimize images (#1)"
    This reverts commit 305c626cb4bdc9e9738f00c1aabfe8546631da95.

--- remix.config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  compression: true,
-  fontOptimization: true,
-  progressiveWebApp: false,
-  imagesOptimization: false,
-  // alias: { "@globals": "/src/globals" },
-  // autoExports: ["globals"],
-};
--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import Router from "@/router/Router";
-import { createRoot } from "react-dom/client";
-
-const root = document.getElementById("root");
-
-createRoot(root).render(<Router />);
